c44f0aaddbfcb1779b9e5ff2d888a046b4a7cde5
Luca Bulles <lucabulles@gmail.com>
2022-10-04 16:42:20 +0200

    Added CRUD+ with Entity Framework for contendb. Need to refactor to suit repository pattern

---
+++ ContentAPI/ContentAPI/Controllers/ContentController.cs
@@ -0,0 +1,81 @@
+﻿using ContentAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Content>>> GetAllContent()
+        {
+            return Ok(await _context.Contents.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Content>> GetContentById(int id)
+        {
+            var content = await _context.Contents.FindAsync(id);
+            if (content == null)
+            {
+                return BadRequest("Content not found");
+            }
+            return Ok(content);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Content>>> PostContent(Content content)
+        {
+            _context.Contents.Add(content);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Contents.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Content>>> UpdateContent(Content request)
+        {
+            var dbContent = await _context.Contents.FindAsync(request.ContentId);
+            if (dbContent == null)
+            {
+                return BadRequest("Content not found.");
+            }
+
+            dbContent.Category = request.Category;
+            dbContent.Name = request.Name;
+            dbContent.Subject = request.Subject;
+            dbContent.Description = request.Description;
+            dbContent.Cast = request.Cast;
+            dbContent.Duration = request.Duration;
+            dbContent.Year = request.Year;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Contents.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Content>>> DeleteContent(int id)
+        {
+            var dbContent = await _context.Contents.FindAsync(id);
+            if (dbContent == null)
+            {
+                return BadRequest("Content not found.");
+            }
+
+            _context.Contents.Remove(dbContent);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Contents.ToListAsync());
+        }
+    }
+}
---
+++ ContentAPI/ContentAPI/Models/Content.cs
@@ -0,0 +1,14 @@
+﻿namespace ContentAPI.Models
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Cast { get; set; } = string.Empty;
+        public string Duration { get; set; } = string.Empty;    
+        public int Year { get; set; }
+    }
+}
