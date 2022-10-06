c22582f6cfcc80d8fafce30a8186d287bbca24c6
Luca Bulles <lucabulles@gmail.com>
2022-10-06 11:03:51 +0200

    added Ocelot API Gateway for Content Microservice

---
+++ ContentAPI/ContentAPI/Repository/IContentRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContentAPI.Models;
+
+namespace ContentAPI
+{
+    public interface IContentRepository
+    {
+        Task<List<Content>> GetAllContent();
+        Task<Content> GetContentById(int id);
+        Task<IEnumerable<Content>> PostContent(Content content);
+        Task<IEnumerable<Content>> UpdateContent(Content request);
+        void DeleteContent(int id);
+
+    }
+}
---
+++ OcelotAPIGateway/Article.API/Controllers/ArticlesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Article.API.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
