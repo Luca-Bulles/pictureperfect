eb70edcd9ec4c9aa684bc91445146f517a961835
Luca Bulles <lucabulles@gmail.com>
2022-10-06 17:45:13 +0200

    Added Review MS

---
+++ ReviewAPI/ReviewAPI/Migrations/20221006153716_CreateInitial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewAPI.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReviewTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReviewContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reported = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
---
+++ ReviewAPI/ReviewAPI/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace ReviewAPI.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewTitle { get; set; } = string.Empty;
+        public string ReviewContent { get; set; } = string.Empty ;
+        public string Reported { get; set; } = string.Empty;
+    }
+}
